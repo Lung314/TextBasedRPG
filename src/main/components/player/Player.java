f6da61f676511755e7479c3febcf5623d3881ee0
Lung314 <Lungya@gmail.com>
2018-09-25 07:26:57 -0500

    Player Class Edits
    Optimized constructors, added 2 different variations of each attribute (max and current), added statUpdate method, added levelUP method, added getter methods.

--- src/main/components/player/Player.java
+++ src/main/components/player/Player.java
@@ -10,19 +10,64 @@ package main.components.player;
 public class Player {
 	private String name;
 	private Job job;
-	private int hp, str, def;
+	private static final int HP_LVLUP_MULTIPLIER = 10, STR_LVLUP_MULTIPLIER = 12, DEF_LVLUP_MULTIPLIER = 13; // the greater the value, the lower the stat growth
+	private int base_hp, base_str, base_def,
+				max_hp, max_str, max_def,
+				current_hp, current_str, current_def,
+				lvl;
 	
 	public Player() {
-		this.name = "Lung";
-		this.job = new Warrior();
-		hp = 10;
-		str = 2;
-		def = 2;
+		this("Lung", new Warrior());
 	}
 	
 	public Player (String name, Job job) {
-		this();
 		this.name = name;
 		this.job = job;
+		base_hp = 10;
+		base_str = 2;
+		base_def = 2;
+		statUpdate();
+		lvl = 1;
+	}
+	
+	public void statUpdate() {
+		max_hp = (int) (base_hp * job.getHPMultiplier());
+		max_str = (int) (base_str * job.getSTRMultiplier());
+		max_def = (int) (base_def * job.getDEFMultiplier());
+	}
+	
+	public void levelUp() {
+		lvl++;
+		base_hp += (int) (  (Math.log(lvl) * lvl)  +  (base_hp / (HP_LVLUP_MULTIPLIER*lvl))  );
+		base_str += (int) (  (Math.log(lvl) * lvl)  +  (base_str / (STR_LVLUP_MULTIPLIER*lvl))  );;
+		base_def += (int) (  (Math.log(lvl) * lvl)  +  (base_hp / (DEF_LVLUP_MULTIPLIER*lvl))  );;
+		statUpdate();
+		current_hp = max_hp;
+		current_str = max_str;
+		current_def = max_def;
+	}
+	
+	public int getMaxHP() {
+		return max_hp;
+	}
+	
+	public int getCurrentHP() {
+		return current_hp;
+	}
+	
+	public int getMaxSTR() {
+		return max_str;
+	}
+	
+	public int getCurrentSTR() {
+		return current_str;
+	}
+	
+	public int getMaxDEF() {
+		return max_def;
+	}
+	
+	public int getCurrentDEF() {
+		return current_def;
 	}
 }
